b5ef167630606f6ba74043d54f096a2e4121aa89
Evgeniia Mokhova <kochetkovaev11@gmail.com>
2024-10-15 19:40:34 +0300

    feat: add button component

---
+++ app/components/Button/Button.props.ts
@@ -0,0 +1,7 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+export interface ButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  children: ReactNode;
+  appearance: 'primary' | 'ghost';
+  arrow?: 'right' | 'down' | 'none';
+}
